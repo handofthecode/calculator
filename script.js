94764823942a6b2801cc542067642b471d5b4f03
Tovi Newman <deartovi@gmail.com>
2017-10-17 12:58:02 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+calculator = {
+  registerHandlers: function() {
+    this.$num.on('click', this.handleNumber.bind(this));
+    this.$op.on('click', this.handleOperator.bind(this));
+    this.$equals.on('click',  this.handleCalculate.bind(this));
+    this.$ce.on('click', this.handleClearEntry.bind(this));
+    this.$c.on('click',  this.handleClearAll.bind(this));
+  },
+  handleCalculate: function() {
+    this.$result.html(eval(this.$equation.html() + this.$result.html()));
+    this.clearEquation();
+  },
+  handleClearEntry: function() {
+    this.$result.html('');
+  },
+  handleClearAll: function() {
+    this.clearEquation();
+    this.handleClearEntry('');
+  },
+  clearEquation: function() {
+    this.$equation.html('');
+  },
+  handleNumber: function(e) {
+    var digit = e.target.innerHTML
+    this.buildNumber(digit)
+  },
+  handleOperator: function(e) {
+    var operator = e.target.innerHTML;
+    this.buildEquation(operator);
+  },
+  buildNumber: function(digit) {
+    this.$result.html(this.$result.html() + digit);
+  },
+  buildEquation: function(operator) {
+    var number = this.$result.html();
+    var previousEquation = this.$equation.html();
+    if (!!number) {
+      this.$equation.html(previousEquation + number + ' ' + operator + ' ');
+      this.$result.html('');
+    }
+  },
+  init: function() {
+    this.$ce = $('#ce');
+    this.$c = $('#c');
+    this.$num = $('.num');
+    this.$op = $('.op');
+    this.$result = $('#result');
+    this.$equation = $('#equation');
+    this.$equals = $('#equals');
+    this.registerHandlers();
+  }
+}
+
+calculator.init();
